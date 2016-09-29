93a0d8c1babda6d97b94e4774dd19cce10721b0e
sajarufe <sajarufe@uc.cl>
2016-09-29 12:59:37 -0300

    Update movie.rb

--- movie.rb
+++ movie.rb
@@ -1,12 +1,51 @@
 # A movie that can be rented
 class Movie
-  REGULAR = 0
-  NEW_RELEASE = 1
-  CHILDRENS = 2
 
   attr_reader :title, :price_code
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def initialize(title)
+    @title = title
   end 
+  
+  def get_amount(days_rented)
+  end
 end
+
+class RegularMovie < Movie
+  def initialize
+    super
+    @price_code = 0
+  end
+  
+  def get_amount(days_rented)
+    this_amount += 2
+    this_amount += (days_rented - 2) * 1.5 if days_rented > 2
+    this_amount
+  end
+end
+
+class NewMovie < Movie
+  def initialize
+    super
+    @price_code = 1
+  end
+  
+ def get_amount(days_rented)
+  this_amount += days_rented * 3
+  this_amount
+ end
+end
+
+class ChildrensMovie < Movie
+  def initialize
+    super
+    @price_code = 2
+  end
+  
+ def get_amount(days_rented)
+  this_amount += 1.5
+  this_amount += (days_rented - 3) * 1.5 if days_rented > 3
+  this_amount
+  end
+end
+  
